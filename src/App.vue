355baf57e91ad99662823e906861c862ee655e45
Cattit <43084768+Cattit@users.noreply.github.com>
2019-11-26 13:38:25 +0300

    fix bags

--- src/App.vue
+++ src/App.vue
@@ -7,68 +7,82 @@
       <v-data-table
         @click:row="editItem"
         :headers="headers"
-        :items="data"
+        :items="items"
         :items-per-page="10"
         :search="search"
         class="elevation-1"
       >
         <template v-slot:top>
           <v-dialog v-model="dialog" max-width="500px">
-            <v-card>
-              <v-card-title>
-                <span class="headline">Изменить данные</span>
-              </v-card-title>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Изменить данные</span>
+                </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6">
-                      <p>Статус: {{editedItem.carStatus}}</p>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <p>Номер вагона: {{editedItem.carNumber}}</p>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <p>stateId: {{editedItem.stateId}}</p>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <v-text-field v-model="editedItem.trainNumber" label="Номер поезда"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <v-text-field v-model="editedItem.lastOperDt" label="Дата-время"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <v-text-field v-model="editedItem.invoiceNumber" label="№ накладной"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                      <v-text-field v-model="editedItem.invoiceId" label="ИД накладной"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6">
+                        <p>Статус: {{editedItem.carStatus}}</p>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <p>Номер вагона: {{editedItem.carNumber}}</p>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <p>stateId: {{editedItem.stateId}}</p>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field v-model="editedItem.trainNumber" label="Номер поезда"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field
+                          :rules="dateRules"
+                          v-model="editedItem.lastOperDt"
+                          label="Дата-время"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field v-model="editedItem.invoiceNumber" label="№ накладной"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <v-text-field v-model="editedItem.invoiceId" label="ИД накладной"></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Отмена</v-btn>
-                <v-btn color="blue darken-1" text @click="save">OK</v-btn>
-              </v-card-actions>
-            </v-card>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Отмена</v-btn>
+                  <v-btn color="blue darken-1" :disabled="!valid" text @click="save">OK</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-form>
           </v-dialog>
         </template>
-        <template v-slot:item.lastOperDt="{ item }">{{ formatDate(item.lastOperDt) }}</template>
       </v-data-table>
     </v-app>
   </div>
 </template>
 
 <script>
-import data from "./table-data.json";
+import testData from "./table-data.json";
+import moment from "moment";
 
 export default {
   name: "App",
+  // начальные данные
   data() {
     return {
-      selected: [],
+      valid: true,
+      // проверка формата даты
+      dateRules: [
+        v =>
+          v === "-" ||
+          moment(v, "DD.MM.YYYY HH:mm:ss", true).isValid() ||
+          "Date must be valid"
+      ],
       search: "",
       dialog: false,
       editedIndex: -1,
@@ -78,12 +92,7 @@ export default {
         invoiceNumber: 0,
         invoiceId: 0
       },
-      defaultItem: {
-        lastOperDt: "",
-        trainNumber: 0,
-        invoiceNumber: 0,
-        invoiceId: 0
-      },
+      // заголовки
       headers: [
         {
           text: "№ n/n",
@@ -98,7 +107,8 @@ export default {
         { text: "ИД накладной", value: "invoiceId" },
         { text: "stateId", value: "stateId" }
       ],
-      data
+      // тело таблицы
+      items: this.formatAllDates(testData)
     };
   },
   watch: {
@@ -108,20 +118,41 @@ export default {
   },
   methods: {
     editItem(item) {
-      this.editedIndex = this.data.indexOf(item);
+      this.editedIndex = this.items.indexOf(item);
       this.editedItem = { ...item };
       this.dialog = true;
     },
+    // приведение даты к нужному формату
     formatDate(date) {
-      if (date == null) return "–";
-      return new Date(date).toLocaleString("ru-RU");
+      if (!date) return "-";
+      return moment(date).format("DD.MM.YYYY HH:mm:ss");
     },
+    formatAllDates(items) {
+      return items.map(item => ({
+        ...item,
+        lastOperDt: this.formatDate(item.lastOperDt)
+      }));
+    },
+    // работа с модальным окном
     close() {
       this.dialog = false;
-      this.editedItem = { ...this.defaultItem };
+      this.editedItem = {
+        lastOperDt: "",
+        trainNumber: 0,
+        invoiceNumber: 0,
+        invoiceId: 0
+      };
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
     },
     save() {
-      Object.assign(this.data[this.editedIndex], this.editedItem);
+      this.validate();
+      this.items = this.items.map((item, index) =>
+        index === this.editedIndex ? this.editedItem : item
+      );
       this.close();
     }
   }
